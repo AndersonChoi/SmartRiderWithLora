b1d7f21c88cac0da36ca38d141511da010d3bd8f
Changhyun-insa <dlstk28@daum.net>
2016-09-04 13:39:02 +0900

    Create end_device.js

---
+++ DBInterfaceWorkspace/models/end_device.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var end_deviceSchema = new Schema({
+	end_device_id: Number,
+	latitude: String,
+	longitude: String,
+	acceleration: String,
+	state: String,
+	time_stamp: Date
+},{
+	versionKey: false
+});
+
+module.exports = mongoose.model('end_device', end_deviceSchema);
