3dc05bc5930c9cf21cc2495b91faf1071f238047
HackerAnderson <choco4235@pusan.ac.kr>
2016-09-04 12:23:48 +0900

    add chart and redesign website

--- AndroidWorkspace/SRapp/app/src/main/java/riders/gumjung/smart/smartridingservice/SignUpActivity.java
+++ AndroidWorkspace/SRapp/app/src/main/java/riders/gumjung/smart/smartridingservice/SignUpActivity.java
@@ -13,7 +13,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
 
     }
 }
