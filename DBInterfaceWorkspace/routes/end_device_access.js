1a94f221eb57ad8b87abf743747098660327f8be
Changhyun-insa <dlstk28@daum.net>
2016-09-10 17:59:57 +0900

    Update and rename end_device.js to end_device_access.js

--- DBInterfaceWorkspace/routes/end_device_access.js
+++ DBInterfaceWorkspace/routes/end_device_access.js
@@ -1,20 +1,19 @@
 module.exports = function(app,End_device){
 	//ADD END DEVICE
 	app.post('/api/end_devices', function(req, res){
-		if(!req.body.end_device_id){
-			var end_device = new End_device();
+		if(!req.body.end_device_id){				//if end_device_id is null
+			var end_device = new End_device();		//auto increase
 			End_device.findOne().sort({id : -1}).exec(function(err, res){
 				if(!res){
-					end_device.id = 1;
+					end_device.end_device_id = 1;
 				}else{
-					end_device.id = geofences.id + 1;
+					end_device.end_device_id = res.end_device_id + 1;
 				}
 
-				end_device.end_device_id = req.body.end_device_id;
 				end_device.latitude = req.body.latitude;
 				end_device.longitude = req.body.longitude;
 				end_device.acceleration = req.body.acceleration;
-				end_device.state = req.body.state;
+				end_device.tracking_count = 0;
 				end_device.time_stamp = req.body.time_stamp;
 
 				end_device.save(function(err){
@@ -26,7 +25,7 @@ module.exports = function(app,End_device){
 					res.json({result: 1});
 				});
 			});
-		}else{
+		}else{										//if end_device_id is not null -> assign
 			End_device.findOne({end_device_id : req.body.end_device_id}, function(err, member){
 				if(!member){
 					var end_device = new End_device();
@@ -35,7 +34,7 @@ module.exports = function(app,End_device){
 					end_device.latitude = req.body.latitude;
 					end_device.longitude = req.body.longitude;
 					end_device.acceleration = req.body.acceleration;
-					end_device.state = req.body.state;
+					end_device.tracking_count = 0;
 					end_device.time_stamp = req.body.time_stamp;
 
 					end_device.save(function(err){
@@ -52,4 +51,12 @@ module.exports = function(app,End_device){
 			});
 		}
     });
+
+	//ADD END DEVICE
+	app.get('/api/end_devices', function(req, res){
+		End_device.find(function(err, end_devices){
+			if(err) return res.status(500).send({error: 'database failure'});
+			res.json(end_devices);
+		});
+	});
 }
