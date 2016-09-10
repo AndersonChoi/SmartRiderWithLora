9bfa551ef4873593f410994dbb21af89bab6cc05
Changhyun-insa <dlstk28@daum.net>
2016-09-10 17:58:01 +0900

    Update app.js

--- DBInterfaceWorkspace/app.js
+++ DBInterfaceWorkspace/app.js
@@ -10,18 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var End_device = require(__dirname + '/models/end_device')
-	, router_end_device = require(__dirname + '/routes/end_device')(app,End_device);
+	, router_end_device = require(__dirname + '/routes/end_device_access')(app,End_device);
 
 var Member = require(__dirname + '/models/member')
-	, router_member = require(__dirname + '/routes/member')(app,Member,End_device);
+	, router_member = require(__dirname + '/routes/member_access')(app,Member,End_device);
 
 app.get('/', function (req, res) {
-	res.send('How to use?<br/><pre>'	
-			 +'<ul><li>[POST]\t /api/members\t\t\t\t\t Add member'
-			 +'<br/>\t\tbody type:\t\tid<br/>\t\t\t\t\tpasswd<br/>\t\t\t\t\tend_device_id</li>'
-			 +'<li>[GET]\t\t /api/members\t\t\t\t\t Lists of members</li>'
-			 +'<li>[GET]\t\t /api/members/:member_id\t\t Info of selected member_id</li>'
-			 +'<li>[DELETE]\t /api/members\t\t\t\t\t Delete the member</li></ul></pre>');
+	res.send('How to use? <a href="https://github.com/AndersonChoi/SmartRiderWithLora/wiki/SR-DB-Interface" target="_blank">Here</a>');
 });
 
 server.listen(appport, function() {
