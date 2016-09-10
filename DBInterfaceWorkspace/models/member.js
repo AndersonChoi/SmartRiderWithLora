f6e33fe003479fd9b11994dc300b3a446f9571a7
Changhyun-insa <dlstk28@daum.net>
2016-09-10 18:00:59 +0900

    Update member.js

--- DBInterfaceWorkspace/models/member.js
+++ DBInterfaceWorkspace/models/member.js
@@ -5,11 +5,13 @@ var memberSchema = new Schema({
 	id: String,
 	password: String,
 	sns: String,
-	end_device_id: Number,
 	start_latitude: String,
 	start_longitude: String,
 	end_latitude: String,
-	end_longitude: String
+	end_longitude: String,
+	end_device_id: Number,
+	tracking_count: Number,
+	tracking_flag: Boolean
 },{
 	versionKey: false
 });
