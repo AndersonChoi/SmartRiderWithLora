8809fa7ab22741e6a2a6c027effa603ea37bad68
Changhyun-insa <dlstk28@daum.net>
2016-09-22 18:21:52 +0900

    Update app.js

--- DBInterfaceWorkspace/app.js
+++ DBInterfaceWorkspace/app.js
@@ -1,26 +1,45 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
 var express = require('express')
-	, http = require('http')
-	, app = express()
-	, server = http.createServer(app)
 	, bodyParser = require('body-parser')
-	, mongoose = require('mongoose')
-	, appport = process.env.VCAP_APP_PORT || 8888;
+	, mongoose = require('mongoose');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+//app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var End_device = require(__dirname + '/models/end_device')
-	, router_end_device = require(__dirname + '/routes/end_device_access')(app,End_device);
+, router_end_device = require(__dirname + '/routes/end_device_access')(app,End_device);
 
 var Member = require(__dirname + '/models/member')
-	, router_member = require(__dirname + '/routes/member_access')(app,Member,End_device);
+, router_member = require(__dirname + '/routes/member_access')(app,Member,End_device);
 
 app.get('/', function (req, res) {
-	res.send('How to use? <a href="https://github.com/AndersonChoi/SmartRiderWithLora/wiki/SR-DB-Interface" target="_blank">Here</a>');
+res.send('How to use? <a href="https://github.com/AndersonChoi/SmartRiderWithLora/wiki/SR-DB-Interface" target="_blank">Here</a>');
 });
 
-server.listen(appport, function() {
-	console.log('Express server listening on port ' + server.address().port);
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
 });
 
 var db = mongoose.connection;
