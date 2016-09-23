48e7f183849c17df57dc0c719810f9afe64fa013
Changhyun-insa <dlstk28@daum.net>
2016-09-23 19:18:04 +0900

    Update member.js

--- DBInterfaceWorkspace/models/member.js
+++ DBInterfaceWorkspace/models/member.js
@@ -3,15 +3,14 @@ var Schema = mongoose.Schema;
 
 var memberSchema = new Schema({
 	id: String,
-	password: String,
 	sns: String,
-	start_latitude: String,
-	start_longitude: String,
-	end_latitude: String,
-	end_longitude: String,
-	end_device_id: Number,
-	tracking_count: Number,
-	tracking_flag: Boolean
+	start_latitude: { type: String, default: "" },
+	start_longitude: { type: String, default: "" },
+	end_latitude: { type: String, default: "" },
+	end_longitude: { type: String, default: "" },
+	end_device_id: { type: Number, default: null },
+	tracking_count: { type: Number, default: 0 },
+	tracking_flag: { type: Boolean, default: false }
 },{
 	versionKey: false
 });
